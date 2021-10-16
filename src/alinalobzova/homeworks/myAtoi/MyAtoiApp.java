26b4e73dd2ece43841b301bcff95f56c68a99a53
alinalobzova <alina_zarova1990@inbox.lv>
2021-10-17 00:14:09 +0300

    MyAtoi

---
+++ src/alinalobzova/homeworks/myAtoi/MyAtoiApp.java
@@ -0,0 +1,26 @@
+public class MyAtoiApp {
+//this implementation doesn't handle corner cases
+    static int atoi(String str) {
+        int result = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c >= '0' && c <= '9') {
+                result = result * 10 + c - '0';
+            }
+        }
+
+        if (str.charAt(0) == '-') {
+            result *= -1;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(atoi("357")); // 123
+        System.out.println(atoi("+735")); // 123
+        System.out.println(atoi("-573")); // -123
+        System.out.println(atoi("+0")); // 0
+        System.out.println(atoi("10")); // 10
+        System.out.println(atoi("-9")); // -9
+    }
+}
